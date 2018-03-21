048386ef995a75424d507e74b385a96c92dba1db
DIR\abdullah.cagatay <abdullah.cagatay@accenture.com>
2018-03-21 16:14:49 +0300

    add css scss files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage.jsx";
 import LoginPage from "./pages/Login/LoginPage.jsx";
 import WildCard from "./pages/WildCard/WildCard.jsx";
+import "bootstrap/dist/css/bootstrap.css";
 
 class App extends Component {
 	render() {
