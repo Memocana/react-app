3fc5967688041107757022234deb020570cb85f3
DIR\abdullah.cagatay <abdullah.cagatay@accenture.com>
2018-03-21 17:22:16 +0300

    redux-integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import {createStore, applyMiddleware, compose} from "redux";
+import {Provider} from "react-redux";
+
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import rootReducer from "./reducers"
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const StoreInstance = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<Provider store={StoreInstance}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById("root")
 );
