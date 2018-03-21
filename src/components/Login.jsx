9a0ae282234037b2551a670ec64efea061db18ff
DIR\abdullah.cagatay <abdullah.cagatay@accenture.com>
2018-03-21 17:01:47 +0300

    login imlementation is started

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import  PropTypes  from  'prop-types';
 
 class Login extends Component {
 	componentWillReceiveProps(nextProps) {
@@ -6,6 +7,7 @@ class Login extends Component {
 	}
 
 	render() {
+		const { name, lastName, validation, selectedHome, houses } = this.props;
 		return (
 			<div className="login-page">
 				<div className="title">
@@ -18,18 +20,45 @@ class Login extends Component {
 				<div className="login-form">
 					<div className="loader-container">
 					</div>
-					<input type="text" placeholder="Ad" name='name' />
-					<input type="text" placeholder="Soyad" name='surName' />
-					<select>
-						<option selected disabled>Ev Seçiniz</option>
+					<input className={validation && !name ? "common-input-error" : "common-input"} type="text" placeholder="Ad" name='name' value={name} onChange={(e) => this.props.handleChange("name", e.target.value)} />
+					{
+						validation  && !name  &&
+						<div  className="help-block">İsim girmelisiniz !</div>
+					}
+					<input className={validation && !lastName ? "common-input-error" : "common-input"} type="text" placeholder="Soyad" name='lastName' value={lastName} onChange={(e) => this.props.handleChange("lastName", e.target.value)} />
+					{
+						validation  && !lastName  &&
+						<div  className="help-block">Soyadı Girmelisiniz !</div>
+					}
+					<select onChange={(e) => this.props.handleChange("selectedHome", e.target.value)}  className= {validation && !selectedHome ? "common-input-error" : "common-input"} >
+						{!selectedHome  ?
+							<option  selected  disabled>Ev Seçiniz</option>
+							:  null}
+						{houses.map((house,  i) => {
+							return  <option  key={i}  selected={selectedHome.id  === house.id}  value={house.id}>{house.name}</option>
+						})}
 					</select>
+					{
+						validation  && !selectedHome  &&
+						<div  className="help-block">Ev seçmelisiniz !</div>
+					}
 					<div className="login-button-container">
-						<button className="login-button">GİRİŞ</button>
+						<button onClick={this.props.login} className="login-button">GİRİŞ</button>
 					</div>
 				</div>
 			</div>
 		);
 	}
+}
+
+Login.propTypes = {
+	handleChange: PropTypes.func,
+	name: PropTypes.string,
+	lastName: PropTypes.string,
+	validation: PropTypes.bool,
+	houses: PropTypes.array,
+	selectedHome: PropTypes.string,
+	login:PropTypes.func
 }
 
 export default Login;
--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,17 +1,53 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Login from "../../components/Login"
 import './LoginPage.scss';
 
+const houses=[
+	{
+		id:1,
+		name:"Accenture"
+	},
+	{
+		id:2,
+		name:"Industry x.0"
+	},
+	{
+		id:3,
+		name:"COE"
+	}
+];
 class LoginPage extends Component {
+  state = {
+    name: '',
+	lastName: '',
+    selectedHome: "",
+    validation: false
+  }
+	handleChange=(name, value) => {
+		let newState =  {...this.state};
+		newState.validation=false;
+		newState[name]=value;
+		this.setState({...newState});
+	}
 	login = () => {
-		this.props.history.push("/home");
-	};
+		console.log("Login triggered");
+		this.setState({validation:true});
+	}
 
 	render() {
+		const {name, lastName, validation, selectedHome }=this.state;
 		return (
-			<div>
-				<h1>Login</h1>
-				<button onClick={e => this.login(e)}>Login</button>
+			<div className={"page-container"}>
+				<Login
+					login={this.login}
+					handleChange={this.handleChange}
+					name={name}
+					lastName={lastName}
+					validation={validation}
+					houses={houses}
+					selectedHome={selectedHome}
+				/>
 			</div>
 		);
 	}
