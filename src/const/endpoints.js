11b244f4265e938f2b3b432567f799f225cf57d9
Zehra Karakas <zehrakarakas2@gmail.com>
2018-03-23 17:38:55 +0300

    Change base url

--- src/const/endpoints.js
+++ src/const/endpoints.js
@@ -1,7 +1,8 @@
-export const baseUrl = "http://ec2-18-195-191-91.eu-central-1.compute.amazonaws.com:8787";
+export const baseUrl = "http://ec2-18-197-160-21.eu-central-1.compute.amazonaws.com:8787";
 export const getAllHouses = "/houses";
 export const registerAndLogin = "/login";
 export const getUsersByHouseId = "/houses/%houseID%/users";
 export const getTasksByHouseId = "/houses/%houseID%/tasks";
 export const deleteTaskById = "/tasks/%taskID%";
 export const addNewTask = "/tasks";
+
